b5c767e38914d40bb3cfff4d9529bbaa0a451069
Belchior Oliveira <96705488+promyr@users.noreply.github.com>
2024-06-09 17:43:25 -0300

    Adicionado Projeto Modelando Iphone

---
+++ Tipos e variaveis/src/Opereadores.java
@@ -0,0 +1,17 @@
+public class Opereadores {
+
+    public static void main(String[] args) {
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condicoes são verdadeiras");           
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condicoes sao verdadeiras");
+            
+        }
+    }
+}
